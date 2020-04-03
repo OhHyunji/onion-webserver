59e0c79791a22dfbb71ba037e94a0b96105c30d3
HyunjiOh <hyunji9212@gmail.com>
2020-04-04 00:06:37 +0900

    3.4.3.1 index.html 응답하기(p.96)

--- src/main/java/RequestHandler.java
+++ src/main/java/RequestHandler.java
@@ -1,14 +1,24 @@
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.base.Strings;
+
 public class RequestHandler extends Thread {
 	private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
+	private static final String WEBAPP_ROOT_PATH = "./webapp";
+	private static final String ROOT_PATH = "/";
+	private static final String DEFAULT_PATH = "/index.html";
 
 	private Socket connection;
 
@@ -20,9 +30,28 @@ public class RequestHandler extends Thread {
 		log.debug("New Client Connected. IP: {}, Port: {}", connection.getInetAddress(), connection.getPort());
 
 		try(InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-			// TODO 사용자 요청에 대한 처리 구현
+			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+
+			String filePath = DEFAULT_PATH;
+
+			while (true) {
+				String line = bufferedReader.readLine();
+
+				if(Strings.isNullOrEmpty(line)) {
+					break;
+				}
+
+				/* startLine: GET /index.html HTTP/1.1 */
+				if(line.contains("GET")) {
+					String[] token = line.split(" ");
+					String path = token[1];
+					filePath = path.equals(ROOT_PATH) ? DEFAULT_PATH : path;
+				}
+			}
+
+			byte[] body = Files.readAllBytes(Paths.get(WEBAPP_ROOT_PATH + filePath));
+
 			DataOutputStream dos = new DataOutputStream(out);
-			byte[] body = "Hello World".getBytes();
 			response200Header(dos, body.length);
 			responseBody(dos,body);
 		} catch (IOException e) {

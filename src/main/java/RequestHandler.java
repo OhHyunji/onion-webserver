e8cba4d9b1e446f8c40ba2b1c0a7aa91d76c0d16
HyunjiOh <hyunji9212@gmail.com>
2020-04-04 02:18:36 +0900

    3.4.3.4 302 status code 적용(p.99)

--- src/main/java/RequestHandler.java
+++ src/main/java/RequestHandler.java
@@ -60,12 +60,15 @@ public class RequestHandler extends Thread {
 				Map<String, String> params = HttpRequestUtils.parseQueryString(body);
 				User user = new User(params.get("userId"), params.get("password"), params.get("name"), params.get("email"));
 				log.debug(user.toString());
-			} else {
-				byte[] body = Files.readAllBytes(Paths.get(WEBAPP_ROOT_PATH + tokens[1]));
 				DataOutputStream dos = new DataOutputStream(out);
-				response200Header(dos, body.length);
-				responseBody(dos,body);
+				response302Header(dos, "/index.html");
 			}
+
+			byte[] body = Files.readAllBytes(Paths.get(WEBAPP_ROOT_PATH + url));
+			DataOutputStream dos = new DataOutputStream(out);
+			response200Header(dos, body.length);
+			responseBody(dos,body);
+
 		} catch (IOException e) {
 			log.error(e.getMessage(), e);
 		}
@@ -76,6 +79,16 @@ public class RequestHandler extends Thread {
 			dos.writeBytes("HTTP/1.1 200 OK \r\n");
 			dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
 			dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+			dos.writeBytes("\r\n");
+		} catch (IOException e) {
+			log.error(e.getMessage(), e);
+		}
+	}
+
+	private void response302Header(DataOutputStream dos, String location) {
+		try {
+			dos.writeBytes("HTTP/1.1 302 Found \r\n");
+			dos.writeBytes("Location: " + location + "\r\n");
 			dos.writeBytes("\r\n");
 		} catch (IOException e) {
 			log.error(e.getMessage(), e);

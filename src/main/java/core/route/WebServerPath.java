cb51d609d5040721f9157fe288f8302b5c9ba4c4
HyunjiOh <hyunji9212@gmail.com>
2020-05-05 17:46:21 +0900

    WebServerPath 정리

--- src/main/java/core/route/WebServerPath.java
+++ src/main/java/core/route/WebServerPath.java
@@ -2,13 +2,14 @@ package core.route;
 
 public class WebServerPath {
     public static final String ROOT = "/";
+    public static final String HOME = "/home";
+
     public static final String USER_CREATE = "/user/create";
     public static final String USER_UPDATE_FORM = "/users/updateForm";
     public static final String USER_UPDATE = "/users/update";
+    public static final String USER_LIST = "/user/list";
+
     public static final String LOGIN = "/users/login";
     public static final String LOGOUT = "/users/logout";
     public static final String LOGIN_FORM = "/users/loginForm";
-    public static final String USER_LIST = "/user/list";
-
-    public static final String HOME = "/home";
 }

e1509c2f039017288972857a9bc0131d7970064e
HyunjiOh <hyunji9212@gmail.com>
2020-05-05 19:53:38 +0900

    6.5 MVC 프레임워크 구현 - RequestMapping

--- src/main/java/servletserver/RequestMapping.java
+++ src/main/java/servletserver/RequestMapping.java
@@ -1,4 +1,36 @@
 package servletserver;
 
+import com.google.common.collect.Maps;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import servletserver.controller.*;
+
+import java.util.Map;
+
 public class RequestMapping {
+    private static final Logger log = LoggerFactory.getLogger(DispatcherServlet.class);
+    private Map<String, Controller> mappings = Maps.newHashMap();
+
+    void initMapping() {
+        mappings.put("/", new ForwardController("/home.jsp"));
+        mappings.put("/users/form", new ForwardController("/user/form.jsp"));
+        mappings.put("/users/loginForm", new ForwardController("/user/login.jsp"));
+        mappings.put("/users", new UserListController());
+        mappings.put("/users/login", new LoginController());
+        mappings.put("/users/profile", new ProfileController());
+        mappings.put("/users/logout", new LogoutController());
+        mappings.put("/users/create", new UserCreateController());
+        mappings.put("/users/updateForm", new UserUpdateFormController());
+        mappings.put("/users/update", new UserUpdateController());
+
+        log.info("###### Initialized Request Mapping ######");
+    }
+
+    public Controller findController(String url) {
+        return mappings.get(url);
+    }
+
+    void put(String url, Controller controller) {
+        mappings.put(url, controller);
+    }
 }
---
+++ src/main/java/servletserver/controller/UserUpdateFormController.java
@@ -0,0 +1,26 @@
+package servletserver.controller;
+
+import core.db.DataBase;
+import core.model.User;
+import servletserver.utils.SessionUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class UserUpdateFormController implements Controller {
+    @Override
+    public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        if(allowUpdate(request)) {
+            User user = DataBase.findByUserId(request.getParameter("userId")).orElseThrow(IllegalStateException::new);
+            request.setAttribute("user", user);
+            return "/user/updateForm.jsp";
+        } else {
+            throw new IllegalStateException();
+        }
+    }
+
+    private boolean allowUpdate(HttpServletRequest req) {
+        User loginUser = SessionUtils.getLoginUser(req).orElseThrow(IllegalStateException::new);
+        return loginUser.getUserId().equals(req.getParameter("userId"));
+    }
+}

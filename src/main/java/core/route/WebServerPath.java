77e49e7a4d39f2d021db605d04168561d592c877
HyunjiOh <hyunji9212@gmail.com>
2020-05-05 17:45:27 +0900

    6.1.3 로그인, 로그아웃 실습(p.193)

--- src/main/java/core/route/WebServerPath.java
+++ src/main/java/core/route/WebServerPath.java
@@ -5,6 +5,10 @@ public class WebServerPath {
     public static final String USER_CREATE = "/user/create";
     public static final String USER_UPDATE_FORM = "/users/updateForm";
     public static final String USER_UPDATE = "/users/update";
-    public static final String LOGIN = "/user/login";
+    public static final String LOGIN = "/users/login";
+    public static final String LOGOUT = "/users/logout";
+    public static final String LOGIN_FORM = "/users/loginForm";
     public static final String USER_LIST = "/user/list";
+
+    public static final String HOME = "/home";
 }
--- src/main/java/servletserver/servlet/LoginServlet.java
+++ src/main/java/servletserver/servlet/LoginServlet.java
@@ -5,6 +5,7 @@ import core.model.User;
 import core.route.WebAppPath;
 import core.route.WebServerPath;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -17,15 +18,25 @@ import java.util.Optional;
 public class LoginServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Optional<User> loginUser = DataBase.findByUserId(req.getParameter("userId"))
-                .filter(u -> u.getPassword().equals(req.getParameter("password")));
+        Optional<User> loginUser = getLoginUser(req.getParameter("userId"), req.getParameter("password"));
 
         if(loginUser.isPresent()) {
+            req.getSession().setAttribute("user", loginUser.get());
             resp.addHeader("Set-Cookie", "login=true");
-            resp.sendRedirect(WebAppPath.DEFAULT_PAGE);
+            resp.sendRedirect(WebServerPath.HOME);
         } else {
             resp.addHeader("Set-Cookie", "login=false");
-            resp.sendRedirect(WebAppPath.LOGIN_FAILED_PAGE);
+            req.setAttribute("loginFailed", true);
+            RequestDispatcher requestDispatcher = req.getRequestDispatcher("/user/login.jsp");
+            requestDispatcher.forward(req, resp);
         }
+    }
+
+    // ---------------------------------------------------------------------------------------------------------------------------------------------------------
+    // -- private
+    // ---------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    private Optional<User> getLoginUser(String userId, String password) {
+        return DataBase.findByUserId(userId).filter(u -> u.getPassword().equals(password));
     }
 }
